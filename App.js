0a38fb6bca72b452df99d5f63919adbd978d02b7
Matan Shaviro <matan3sh@gmail.com>
2021-09-16 19:14:37 +0300

    Learn about device permissions

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as ImagePicker from 'expo-image-picker';
 
 import Screen from './app/components/Screen';
+import { Button, Image } from 'react-native';
 
 export default function App() {
+  const [imageUri, setImageUri] = useState('');
+
   const requestPermission = async () => {
     const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
     if (!granted) alert('You need to enable permission to access the library');
@@ -13,5 +16,19 @@ export default function App() {
     requestPermission();
   }, []);
 
-  return <Screen></Screen>;
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) setImageUri(result.uri);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Screen>
+      <Button title='Select Image' onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
+  );
 }
